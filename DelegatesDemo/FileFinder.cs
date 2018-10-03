9ddb7ae3772d2b743c84d48dcaa476896ce4b3fa
Oscar Bennich-Björkman <W100567@internal.financecorp.biz>
2018-10-03 11:03:19 +0200

    Initial commit

---
+++ DelegatesDemo/FileFinder.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace DelegatesDemo
+{
+    //delegate bool FileFilter(string path);
+
+    static class FileFinder
+    {
+        public static void Find(string dirPath, Func<string, bool> filter)
+        {
+            string[] filePaths = Directory.GetFiles(dirPath);
+            foreach (string filePath in filePaths)
+            {
+                if (filter(filePath))
+                {
+                    Console.WriteLine(filePath);
+                }
+            }
+        }
+    }
+}
---
+++ DelegatesDemo/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.IO;
+
+namespace DelegatesDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string dirPath = @"C:\Users\W100567\Desktop\Textfolder";
+
+            // C# 1.0 (named delegates)
+            FileFinder.Find(dirPath, MyTextFilter);
+
+            // C# 2.0 (anonymous delegates)
+            FileFinder.Find(dirPath, delegate(string filePath)
+            {
+                return string.Equals(Path.GetExtension(filePath), ".txt", 
+                    StringComparison.OrdinalIgnoreCase);
+            });
+
+            // C# 3.0 (lambda expression)
+            FileFinder.Find(dirPath, p => string.Equals(Path.GetExtension(p), ".txt", StringComparison.OrdinalIgnoreCase));
+        }
+
+        static bool MyTextFilter(string filePath)
+        {
+            return string.Equals(Path.GetExtension(filePath), ".txt", StringComparison.OrdinalIgnoreCase);
+        }
+    }
+}
